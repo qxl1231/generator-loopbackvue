959f6d1d7ed96b2a0fc53e59aea10aa28928d320
qiangxl <xiaolong.qiang@midea.com>
2016-11-14 10:31:05 +0800

    yeoman 脚手架-loopback vue

--- generators/index.js
+++ generators/index.js
@@ -7,19 +7,28 @@ var util = require('util');
 var yeoman = require('yeoman-generator');
 var yosay = require('yosay');    //yeoman弹出框
 var path = require('path');
-var Reactpackage = yeoman.Base.extend({
+var loopbackvue = yeoman.Base.extend({
   info: function() {
     this.log(chalk.green(
       'I am going to build your app!'
     ));
   },
   generateBasic: function() {  //按照自己的templates目录自定义
-    this.directory('src', 'src');    //拷贝目录
-    this.directory('data', 'data');
+    this.directory('client', 'client');    //拷贝目录
+    this.directory('common', 'common');
+    this.directory('server', 'server');
     this.copy('package.json', 'package.json');   //拷贝文件
     this.copy('index.html', 'index.html');
     this.copy('README.md', 'README.md');
-    this.copy('webpack.config.js', 'webpack.config.js');
+    this.copy('.editorconfig', '.editorconfig');
+    this.copy('.gitignore', '.gitignore');
+    this.copy('.travis.yml', '.travis.yml');
+    this.copy('jsconfig.json', 'jsconfig.json');
+    this.copy('Wechat.jpeg', 'Wechat.jpeg');
+    this.copy('daovoice.png', 'daovoice.png');
+    this.copy('alipay.jpeg', 'alipay.jpeg');
+    this.copy('ci.sh', 'ci.sh');
+    this.copy('circle.yml', 'circle.yml');
   },
   generateClient: function() {
     this.sourceRoot(path.join(__dirname, 'templates'));
@@ -32,8 +41,8 @@ var Reactpackage = yeoman.Base.extend({
   },
   end: function() {
     this.log(yosay(
-      'Your app has been created successfully!'
+      'Your app has been created successfully!please run "npm install" later'
     ));
   }
 });
-module.exports = Reactpackage;
+module.exports = loopbackvue;
